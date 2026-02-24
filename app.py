23b42efa01813e3174dab9b2df8d37249c33c02f
Saad <wajids@coventry.ac.uk>
2026-02-24 05:41:55 +0300

    Fixing project structure and bugs

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def load_user(user_id):
 
 
 class User(db.Model,UserMixin):
-    Uid = db.Column(db.Integer, nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
@@ -31,15 +31,15 @@ class User(db.Model,UserMixin):
     acc_type = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.Uid}','{self.first_name}', '{self.last_name}')"
+        return f"User('{self.id}','{self.first_name}', '{self.last_name}')"
     
 class Organization(db.Model,UserMixin):
-    Oid = db.Column(db.Integer, nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     og_type = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return f"Organization('{self.Oid}','{self.name}')"
+        return f"Organization('{self.id}','{self.name}')"
 
 with app.app_context():
     if not os.path.exists('komodohub.db'):
@@ -50,6 +50,7 @@ with app.app_context():
 def root():
     return render_template('root.html')
 
+@login_required
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -58,19 +59,19 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html')
+                return redirect(url_for('home'))
+    return render_template('login.html',form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
+        new_user = User(email=form.email.data, first_name=form.first_name.data, last_name=form.last_name.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
