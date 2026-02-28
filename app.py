adeb6d7d000705e80d9737e806685fcabb126cac
mohame219 <mohame219@coventry.ac.uk>
2026-02-28 19:54:49 +0000

    Created ContactMessage model, added contact form validation, database commit handling, and status provision using flashed messages

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, ContactForm
 import os
+from datetime import datetime
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
@@ -11,6 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///komodohub.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+app.secret_key = "secret-key-for-csrf-tokens"
+csrf = CSRFProtect(app)     # enable CSRF protection globally on the 'app' instance
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -41,13 +45,25 @@ class Organization(db.Model,UserMixin):
     def __repr__(self):
         return f"Organization('{self.id}','{self.name}')"
 
+class ContactMessage(db.Model):
+    id = db.Column(db.Integer, nullable = False, primary_key= True)
+    first_name = db.Column(db.String(20), nullable= False)
+    last_name = db.Column(db.String(20), nullable= False)
+    email = db.Column(db.String(120), nullable= False)
+    region = db.Column(db.String(60), nullable= False)
+    userMessage = db.Column(db.Text, nullable= False)
+    time_of_creation = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"ContactMessage({self.first_name} {self.last_name}: {self.userMessage})"
+
 with app.app_context():
     if not os.path.exists('komodohub.db'):
         db.create_all()
 
 
 @app.route('/')
-def root():
+def index():
     return render_template('landing.html')
 
 @app.route('/home')
@@ -76,6 +92,36 @@ def register():
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+@app.route('/contact', methods= ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        contact_message = ContactMessage(first_name= form.first_name.data.strip(),
+                                         last_name= form.last_name.data.strip(),
+                                         region= form.region.data,
+                                         email= form.email.data.strip().lower(),
+                                         userMessage= form.userQuery.data.strip())
+        try:
+            db.session.add(contact_message)      #adds the new data field about a user's query to the db table
+            db.session.commit()
+            """needs to send an email status update instead"""
+            flash("Query Submitted!", "success")
+
+            if current_user.is_authenticated:
+                return redirect(url_for('home'))            # if the user in current session if logged in, redirect to homepage
+            else:
+                return redirect(url_for('index'))           # otherwise redirect to landing page
+
+        except Exception as e:
+            db.session.rollback()
+            """needs to send an email status update instead"""
+            flash("Something went wrong. Please try again", "failure")
+            return redirect(url_for('contact'))
+
+    else:
+        """  show validation errors (in HTML template) in case of form validation failure """
+        return render_template('contactForm.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
