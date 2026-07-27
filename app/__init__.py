7aa6f24f10f7d2607ecbe5bb153204f78b18b56b
Saad Wajid <saadwajid401@gmail.com>
2026-07-27 15:40:32 +0100

    Updated values to use environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,15 @@ from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key-here'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///komodohub.db'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-app.secret_key = "secret-key-for-csrf-tokens"
+app.secret_key = os.environ['SECRET_KEY']
 csrf = CSRFProtect(app)     # enable CSRF protection globally on the 'app' instance
 app.config['WTF_CSRF_ENABLED'] = True
 
@@ -21,14 +23,12 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-load_dotenv()
-
 # email configuration
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv("EMAIL")
-app.config['MAIL_PASSWORD'] = os.getenv("PASSWORD")
+app.config['MAIL_USERNAME'] = os.environ["EMAIL"]
+app.config['MAIL_PASSWORD'] = os.environ["PASSWORD"]
 mail = Mail(app)
 
 from app import routes
